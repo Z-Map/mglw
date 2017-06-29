19406f8f89d71a3829e65ac417e05978cbc86720
Quentin <blendfan@gmail.com>
2017-06-29 18:45:51 +0200

    fix symbol conflict

--- src/include/mgl/drawer.h
+++ src/include/mgl/drawer.h
@@ -6,7 +6,7 @@
 /*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/26 17:36:16 by qloubier          #+#    #+#             */
-/*   Updated: 2017/06/29 12:13:01 by qloubier         ###   ########.fr       */
+/*   Updated: 2017/06/29 18:44:58 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include "mgl/data/rect.h"
 # include "mglw/mgl.h"
 
-v4f				rgbatov4f(unsigned int color);
+v4f				coltov4f(unsigned int color);
 
 
 #endif
